81d076e371bfec637eb647b9c865cf0e8b295862
kzaldasp <kevinaldas290621@gmail.com>
2026-02-19 21:02:57 -0500

    feat: diseño inicial de invitación web con componentes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import icon from 'astro-icon'; // 1. Importamos astro-icon
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [icon()], // 2. Lo agregamos a las integraciones
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
